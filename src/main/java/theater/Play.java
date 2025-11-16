50e2d2a8a954c738c9a7b4a407e94cf0b7623a2f
Zixiao Wang <shawnzixiao.wang@mail.utoronto.ca>
2025-11-16 14:21:09 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,35 @@
 package theater;
 
+/**
+ * Represents a play with a name and type.
+ *
+ * <p>This class does not accept null values for name or type.</p>
+ *
+ * @null No fields in this class may be null.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
+        this.setName(name);
+        this.setType(type);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
         this.type = type;
     }
 }
